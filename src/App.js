9c4be9c3db571d72041c1b3c2a5f4912c49882a8
Roberto Aranda <ese.rober@gmail.com>
2017-12-21 00:24:25 +0100

    Allows to manage other lists manually created

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
-import VideoListContainer from './containers/VideoListContainer'
-import AddItem from './containers/AddItem'
-import SignInBar from './containers/SignInBar'
+import VideoListContainer from './containers/VideoListContainer';
+import AddItem from './containers/AddItem';
+import SignInBar from './containers/SignInBar';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 class App extends Component {
-  
   render() {
     return (
       <div>
         <SignInBar />
         <AddItem />
-        <VideoListContainer />
+        <BrowserRouter>
+          <Route path="/:list?" component={VideoListContainer} />
+        </BrowserRouter>
       </div>
     );
   }
